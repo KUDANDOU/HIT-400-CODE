f06382baa2d6250d127710069a615d99e47bb508
funani <fndou94@gmail.com>
2018-04-18 14:11:16 +0200

    another one

--- file_info.py
+++ file_info.py
@@ -7,4 +7,36 @@ pe = pefile.PE(exe_path)
 # print("[*] Signature value: %s" % hex(pe.NT_HEADERS.Signature))
 
 
-print (pe.FileInfo)
+# print (pe.FileInfo)
+
+
+def get_version_info(pe):
+    """Return version infos"""
+    res = {}
+    for fileinfo in pe.FileInfo:
+        if fileinfo.Key == 'StringFileInfo':
+            for st in fileinfo.StringTable:
+                for entry in st.entries.items():
+                    res[entry[0]] = entry[1]
+        if fileinfo.Key == 'VarFileInfo':
+            for var in fileinfo.Var:
+                res[var.entry.items()[0][0]] = var.entry.items()[0][1]
+    if hasattr(pe, 'VS_FIXEDFILEINFO'):
+        res['flags'] = pe.VS_FIXEDFILEINFO.FileFlags
+        res['os'] = pe.VS_FIXEDFILEINFO.FileOS
+        res['type'] = pe.VS_FIXEDFILEINFO.FileType
+        res['file_version'] = pe.VS_FIXEDFILEINFO.FileVersionLS
+        res['product_version'] = pe.VS_FIXEDFILEINFO.ProductVersionLS
+        res['signature'] = pe.VS_FIXEDFILEINFO.Signature
+        res['struct_version'] = pe.VS_FIXEDFILEINFO.StrucVersion
+    return res
+
+
+data = get_version_info(pe)
+
+print(data)
+
+
+print("[*] Listing imported DLLs...")
+for entry in pe.DIRECTORY_ENTRY_IMPORT:
+    print('\t' + entry.dll.decode('utf-8'))
